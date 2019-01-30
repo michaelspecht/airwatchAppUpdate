db7b8d705e889b02c64a56720e9579eb063c209d
Michael Specht <michael.specht@adesa.com>
2019-01-30 15:46:08 -0500

    remove newline dashes

--- airwatchUpload.sh
+++ airwatchUpload.sh
@@ -65,17 +65,19 @@ openssl base64 -in $3 -out ${base64Binary}
 # ------------------------------------------------------------------
 
 # Put the base64 data in the ChunkData json value
-echo -n "{ \"ChunkData\": \"" > ${uploadJson}
+echo "{ \"ChunkData\": \"" > ${uploadJson}
 cat ${base64Binary} >> ${uploadJson}
 
 # Use a ChunkSequenceNumber of 1 to indicate this is the first upload chunk (and in this case, only upload chunk)
-echo -n "\",\"ChunkSequenceNumber\": 1,\"TotalApplicationSize\":" >> ${uploadJson}
+echo "\",\"ChunkSequenceNumber\": 1,\"TotalApplicationSize\":" >> ${uploadJson}
 
 # Get the total size of the app binary, and use for both TotalApplicationSize and ChunkSize since we upload it all at once
 cat ${base64Binary} | wc -c >> ${uploadJson}
-echo -n ",\"ChunkSize\": " >> ${uploadJson}
+echo ",\"ChunkSize\": " >> ${uploadJson}
 cat ${base64Binary} | wc -c >> ${uploadJson}
-echo -n "}" >> ${uploadJson}
+echo "}" >> ${uploadJson}
+
+cat ${uploadJson}
 
 # Make the POST Request to upload the binary to AirWatch. This will only put the artifact in airwatch, and not make it visible to any users
 curl -vX POST -H "Authorization: Basic $1" -H "aw-tenant-code: $2" "${airwatchUrl}/api/mam/apps/internal/uploadchunk" -d @${uploadJson} --header "Content-Type: application/json" > ${uploadResult}
@@ -100,29 +102,29 @@ fi
 # Add variable groups, error handling, version numbers (and put version number in the app)
 
 # Use the TransactionId from the upload request to associate the install with that uploaded binary
-echo -n "{\"TransactionId\": \"" > ${beginInstallJson}
+echo "{\"TransactionId\": \"" > ${beginInstallJson}
 cat ${uploadResult} | grep -o '"TranscationId":"[a-zA-Z0-9-]*' | awk '{print substr($0,18,length())}' >> ${beginInstallJson}
 
 # Set the device types, file name,
-echo -n "\",\"DeviceType\": \"2\",\"ApplicationName\": \"$4\",\"SupportedModels\": {\"Model\": [" >> ${beginInstallJson}
+echo "\",\"DeviceType\": \"2\",\"ApplicationName\": \"$4\",\"SupportedModels\": {\"Model\": [" >> ${beginInstallJson}
 
 # Model 1 is iPhone, 3 is iPod Touch
 if [ "$8" = "1" ]; then
-echo -n "{\"ApplicationId\": $6,\"ModelId\": 1},{\"ApplicationId\": $6,\"ModelId\": 3}" >> ${beginInstallJson}
+echo "{\"ApplicationId\": $6,\"ModelId\": 1},{\"ApplicationId\": $6,\"ModelId\": 3}" >> ${beginInstallJson}
 fi
 
 # Model 2 is iPad
 if [ "$9" = "1" ]; then
-echo -n ",{\"ApplicationId\": $6,\"ModelId\": 2}" >> ${beginInstallJson}
+echo ",{\"ApplicationId\": $6,\"ModelId\": 2}" >> ${beginInstallJson}
 fi
 
 # Set push mode (auto)
-echo -n "]},\"PushMode\": \"Auto\",\"AutoUpdateVersion\": true,\"FileName\": \"$5\",\"AppVersion\": \"" >> ${beginInstallJson}
+echo "]},\"PushMode\": \"Auto\",\"AutoUpdateVersion\": true,\"FileName\": \"$5\",\"AppVersion\": \"" >> ${beginInstallJson}
 
 # Add the app version
-echo -n $7 | awk '{printf substr($0,3,4); printf "."; printf substr($0,7,7)}' >> ${beginInstallJson}
+echo $7 | awk '{printf substr($0,3,4); printf "."; printf substr($0,7,7)}' >> ${beginInstallJson}
 
-echo -n \"\} >> ${beginInstallJson}
+echo \"\} >> ${beginInstallJson}
 cat ${beginInstallJson}
 
 # ------------------------------------------------------------------
